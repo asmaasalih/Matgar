a34205a303ff29a7ba9957c6d659634af9e7609a
Asmaa Salih <aemam383@gmail.com>
2019-08-01 11:54:48 +0400

    g

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('cart/',include('cart.urls')),
     path('orders/',include('orders.urls')),
